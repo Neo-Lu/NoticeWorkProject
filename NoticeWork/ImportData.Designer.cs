849b0b190f46cbf700cf403608996ac7e621d976
NeoLu <ibliveicando@163.com>
2016-05-23 22:46:36 +0800

    添加导入励志语言功能

---
+++ NoticeWork/ImportData.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace NoticeWork
+{
+    partial class ImportData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImportData = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnImportData
+            // 
+            this.btnImportData.Location = new System.Drawing.Point(177, 179);
+            this.btnImportData.Name = "btnImportData";
+            this.btnImportData.Size = new System.Drawing.Size(198, 135);
+            this.btnImportData.TabIndex = 0;
+            this.btnImportData.Text = "导入数据";
+            this.btnImportData.UseVisualStyleBackColor = true;
+            this.btnImportData.Click += new System.EventHandler(this.btnImportData_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "导入网页的ID:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(177, 122);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(198, 28);
+            this.txtID.TabIndex = 2;
+            // 
+            // ImportData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 493);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnImportData);
+            this.Name = "ImportData";
+            this.Text = "ImportData";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImportData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+    }
+}
---
+++ NoticeWork/ImportData.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Java2Dotnet.Spider.Core;
+using Java2Dotnet.Spider.Core.Downloader;
+using Java2Dotnet.Spider.Core.Pipeline;
+using Java2Dotnet.Spider.Core.Processor;
+using Java2Dotnet.Spider.Core.Selector;
+
+namespace NoticeWork
+{
+    public partial class ImportData : Form
+    {
+        public ImportData()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImportData_Click(object sender, EventArgs e)
+        {
+            string id = txtID.Text.Trim();
+            using (var db=new NeoNoticeWorkEntities())
+            {
+                List<string> nameList = GetHtmlContent(id);
+                foreach (string name in nameList)
+                {
+                    InspirationalQuotes model = new InspirationalQuotes();
+                    model.Name = name;
+                    db.InspirationalQuotes.Add(model);
+                }
+                db.SaveChanges();
+                MessageBox.Show("导入成功！");
+            }
+        }
+
+        private List<string> GetHtmlContent(string id)
+        {
+            List<string> list = new List<string>();
+            HttpClientDownloader downloader = new HttpClientDownloader();
+            /*
+             * http://lizhi.yjbys.com/mingyan/373438.html
+             * http://lizhi.yjbys.com/mingyan/373443.html
+             * http://lizhi.yjbys.com/mingyan/221755.html
+             */
+            string url = "http://lizhi.yjbys.com/mingyan/"+ id + ".html";
+            Java2Dotnet.Spider.Core.Spider spider = Java2Dotnet.Spider.Core.Spider.Create(new Site() { EncodingName = "UTF-8" },
+                new SimplePageProcessor(url, url +"/* ")).AddPipeline(new TestPipeline()).SetThreadNum(1);
+            Page p = downloader.Download(new Request(url, 2, new Dictionary<string, dynamic>()), spider);
+            //Console.WriteLine(p.Content);
+
+            var s = Selectors.Css("div p").SelectList(p.Content);
+            for (int i = 0; i < s.Count; i++)
+            {
+                Match m = Regex.Match(s[i].OuterHtml, @"<p>([\s\S]*)</p>", RegexOptions.IgnoreCase);
+                if (m.Success)
+                {
+                    string result = m.Result("$1").Replace("&mdash;", "—").Replace("&nbsp;", "");
+                    if (result != "")
+                    {
+                        if (!result.Contains("<"))
+                        {
+                            list.Add(result);
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+
+        private class TestPipeline : IPipeline
+        {
+            public void Process(ResultItems resultItems, ISpider spider)
+            {
+                foreach (var entry in resultItems.Results)
+                {
+                    Console.WriteLine($"{entry.Key}:{entry.Value}");
+                }
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
